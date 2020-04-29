dfb65d609fd60e2fb8c3fa349802bc19821497d6
chenyaofo <chenyaofo@gmail.com>
2020-04-29 10:55:34 +0800

    sort out dataset code.

--- core/dataset/cifar.py
+++ core/dataset/cifar.py
@@ -1,12 +1,43 @@
 import torch.utils.data as data
 import torchvision.transforms as T
-from torchvision.datasets import CIFAR10, CIFAR100
+from torchvision.datasets import CIFAR10
 
 from ..transforms import Cutout, CIFAR10Policy
 from ..register import DATASET
 
 
-def get_cifar_train_transform(cutout,autoaugment,mean,std):
+CIFAR10_MEAN = []
+CIFAR10_STD = []
+
+
+class CIFAR10_D:
+    NUM_CLASS = 10
+    @staticmethod
+    def build_train_loader(args):
+        train_transforms = [
+            T.RandomCrop(32, padding=4),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize(mean=CIFAR10_MEAN, std=CIFAR10_STD)
+        ]
+        trainset = CIFAR10(args.data, train=True, transform=train_transforms)
+        train_loader = data.DataLoader(trainset, batch_size=args.batch_size,
+                                       shuffle=True, num_workers=args.workers)
+        return train_loader
+
+    @staticmethod
+    def build_val_loader(args):
+        val_transforms = [
+            T.ToTensor(),
+            T.Normalize(mean=CIFAR10_MEAN, std=CIFAR10_STD)
+        ]
+        valset = CIFAR10(args.data, train=False, transform=val_transforms)
+        val_loader = data.DataLoader(valset, batch_size=args.batch_size,
+                                     shuffle=False, num_workers=args.workers)
+        return val_loader
+
+
+def get_cifar_train_transform(cutout, autoaugment, mean, std):
     transforms = [
         T.RandomCrop(32, padding=4),
         T.RandomHorizontalFlip(),
@@ -20,63 +51,8 @@ def get_cifar_train_transform(cutout,autoaugment,mean,std):
     return transforms
 
 
-def get_cifar_val_transform(mean,std):
+def get_cifar_val_transform(mean, std):
     return T.Compose([
         T.ToTensor(),
         T.Normalize(mean=mean, std=std),
     ])
-
-def get_cifar_trainset(dataset=CIFAR10, root=".data", transform=None, target_transform=None, download=True):
-    ds = dataset(root=root, train=True, transform=transform,
-                 target_transform=target_transform, download=download)
-    return ds
-
-
-def get_cifar_valset(dataset=CIFAR10, root=".data", transform=None, target_transform=None, download=True):
-    ds = dataset(root=root, train=False, transform=transform,
-                 target_transform=target_transform, download=download)
-    return ds
-
-
-def get_cifar_train_loader(dataset=CIFAR10, root=".data",
-                           transform=None, target_transform=None, download=True,
-                           batch_size=128, num_workers=4):
-    ds = get_cifar_trainset(dataset, root, transform,
-                            target_transform, download)
-    return data.DataLoader(dataset=ds, shuffle=True, batch_size=batch_size, num_workers=num_workers)
-
-
-def get_cifar_val_loader(dataset=CIFAR10, root=".data",
-                         transform=None, target_transform=None, download=True,
-                         batch_size=128, num_workers=4):
-    ds = get_cifar_valset(dataset, root, transform,
-                          target_transform, download)
-    return data.DataLoader(dataset=ds, shuffle=False, batch_size=batch_size, num_workers=num_workers)
-
-def _cifar(dataset_type:CIFAR10, cfg):
-    ROOT =cfg["dataset.root"]
-    DATASET_MEAN =  cfg["dataset.mean"]
-    DATASET_STD =  cfg["dataset.std"]
-    CUTOUT = cfg["transforms.cutout"]
-    AUTOAUGMENT = cfg["transforms.autoaugment"]
-    BATCH_SIZE = cfg["strategy.batch_size"]
-    NUM_WORKERS = cfg["machine.num_workers"]
-
-    train_transforms = get_cifar_train_transform(CUTOUT,AUTOAUGMENT,DATASET_MEAN, DATASET_STD)
-    val_transforms = get_cifar_val_transform(DATASET_MEAN, DATASET_STD)
-
-    trainset = dataset_type(root=ROOT, train=True, transform=train_transforms, download=True)
-    valset = dataset_type(root=ROOT, train=False, transform=val_transforms, download=True)
-
-    train_loader = data.DataLoader(dataset=trainset, shuffle=True, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
-    val_loader = data.DataLoader(dataset=valset, shuffle=False, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
-
-    return train_loader,val_loader
-
-@DATASET.register
-def cifar10(cfg):
-    return _cifar(CIFAR10, cfg)
-
-@DATASET.register
-def cifar100(cfg):
-    return _cifar(CIFAR100, cfg)
--- core/dataset/imagenet.py
+++ core/dataset/imagenet.py
@@ -4,61 +4,111 @@ import torch.utils.data as data
 import torchvision.transforms as T
 from torchvision.datasets import ImageFolder
 from torch.utils.data.distributed import DistributedSampler
-from nvidia.dali.plugin.pytorch import DALIClassificationIterator
-
-from utils.distributed import world_size, local_rank
-
-from .dali import HybridTrainPipe, HybridValPipe
-
-IMAGENET_MEAN = [0.485, 0.456, 0.406]
-IMAGENET_STD = [0.229, 0.224, 0.225]
 
 
-def build_imagenet_loader(args):
-    if args.dali:
-        train_pipe = HybridTrainPipe(batch_size=args.batch_size, num_threads=args.num_workers,
-                                    data_dir=os.path.join(args.data, "train"), crop=args.image_size)
+from torchutils.distributed import world_size, local_rank
+
+try:
+    import nvidia.dali.ops as ops
+    import nvidia.dali.types as types
+    from nvidia.dali.pipeline import Pipeline
+    from nvidia.dali.plugin.pytorch import DALIClassificationIterator
+except ImportError:
+    raise ImportError("Please install DALI refer to "
+                      "https://docs.nvidia.com/deeplearning/sdk/dali-developer-guide/docs/installation.html.")
+
+from torchutils.distributed import world_size, local_rank
+
+IMAGENET_MEAN = [0.485*255, 0.456*255, 0.406*255]
+IMAGENET_STD = [0.229*255, 0.224*255, 0.225*255]
+
+
+class HybridTrainPipe(Pipeline):
+    def __init__(self, batch_size, num_threads, data_dir, crop,
+                 device_id=local_rank(), only_cpu=False):
+        super(HybridTrainPipe, self).__init__(batch_size, num_threads, device_id, seed=12 + device_id)
+        self.input = ops.FileReader(file_root=data_dir, shard_id=local_rank(),
+                                    num_shards=world_size(), random_shuffle=True)
+        # let user decide which pipeline works him bets for RN version he runs
+        dali_device = 'cpu' if only_cpu else 'gpu'
+        decoder_device = 'cpu' if only_cpu else 'mixed'
+        # This padding sets the size of the internal nvJPEG buffers to be able to
+        # handle all images from full-sized ImageNet
+        # without additional reallocations
+        device_memory_padding = 211025920 if decoder_device == 'mixed' else 0
+        host_memory_padding = 140544512 if decoder_device == 'mixed' else 0
+        self.decode = ops.ImageDecoderRandomCrop(device=decoder_device, output_type=types.RGB,
+                                                 device_memory_padding=device_memory_padding,
+                                                 host_memory_padding=host_memory_padding,
+                                                 random_aspect_ratio=[0.8, 1.25],
+                                                 random_area=[0.1, 1.0],
+                                                 num_attempts=100)
+        self.resize = ops.Resize(device=dali_device, resize_x=crop, resize_y=crop,
+                                 interp_type=types.INTERP_TRIANGULAR)
+        self.cmnp = ops.CropMirrorNormalize(device=dali_device,
+                                            output_dtype=types.FLOAT,
+                                            output_layout=types.NCHW,
+                                            crop=(crop, crop),
+                                            image_type=types.RGB,
+                                            mean=IMAGENET_MEAN,
+                                            std=IMAGENET_STD)
+        self.coin = ops.CoinFlip(probability=0.5)
+
+    def define_graph(self):
+        rng = self.coin()
+        images, labels = self.input(name="Reader")
+        images = self.decode(images)
+        images = self.resize(images)
+        images = self.cmnp(images.gpu(), mirror=rng)
+        return [images, labels]
+
+
+class HybridValPipe(Pipeline):
+    def __init__(self, batch_size, num_threads, data_dir, crop, size, device_id=local_rank(), only_cpu=False):
+        super(HybridValPipe, self).__init__(batch_size, num_threads, device_id, seed=12 + device_id)
+        self.input = ops.FileReader(file_root=data_dir, shard_id=local_rank(),
+                                    num_shards=world_size(), random_shuffle=False)
+        dali_device = 'cpu' if only_cpu else 'gpu'
+        decoder_device = 'cpu' if only_cpu else 'mixed'
+        self.decode = ops.ImageDecoder(device=decoder_device, output_type=types.RGB)
+        self.resize = ops.Resize(device=dali_device, resize_shorter=size, interp_type=types.INTERP_TRIANGULAR)
+        self.cmnp = ops.CropMirrorNormalize(device=dali_device,
+                                            output_dtype=types.FLOAT,
+                                            output_layout=types.NCHW,
+                                            crop=(crop, crop),
+                                            image_type=types.RGB,
+                                            mean=IMAGENET_MEAN,
+                                            std=IMAGENET_STD)
+
+    def define_graph(self):
+        images, labels = self.input(name="Reader")
+        images = self.decode(images)
+        images = self.resize(images)
+        images = self.cmnp(images)
+        return [images, labels]
+
+
+class ImageNet:
+    NUM_CLASS = 1000
+    @staticmethod
+    def build_train_loader(args):
+        train_pipe = HybridTrainPipe(batch_size=args.batch_size,
+                                     num_threads=args.num_workers,
+                                     data_dir=os.path.join(args.data, "train"),
+                                     crop=args.image_size)
         train_pipe.build()
-        train_loader = DALIClassificationIterator([train_pipe], train_pipe.epoch_size("Reader") / world_size())
+        train_loader = DALIClassificationIterator([train_pipe],
+                                                  train_pipe.epoch_size("Reader") / world_size())
+        return train_loader
 
-        val_pipe = HybridValPipe(batch_size=args.batch_size, num_threads=args.num_workers,
-                                data_dir=os.path.join(args.data, "val"), crop=args.image_size,
-                                size=int(args.image_size/0.875))
+    @staticmethod
+    def build_val_loader(args):
+        val_pipe = HybridValPipe(batch_size=args.batch_size,
+                                 num_threads=args.num_workers,
+                                 data_dir=os.path.join(args.data, "val"),
+                                 crop=args.image_size,
+                                 size=int(args.image_size/0.875))
         val_pipe.build()
-        val_loader = DALIClassificationIterator([val_pipe], val_pipe.epoch_size("Reader") / world_size())
-    else:
-        trainset = ImageFolder(
-            root=os.path.join(args.data, "train"),
-            transform=T.Compose([
-                T.RandomResizedCrop(args.image_size),
-                T.RandomHorizontalFlip(),
-                T.ToTensor(),
-                T.Normalize(IMAGENET_MEAN, IMAGENET_STD),
-            ])
-        )
-        train_sampler = DistributedSampler(trainset)
-
-        train_loader = data.DataLoader(
-            dataset=trainset,
-            batch_size=args.batch_size,
-            sampler=train_sampler,
-            num_workers=args.num_workers,
-        )
-
-        valset = ImageFolder(
-            root=os.path.join(args.data, "val"),
-            transform=T.Compose([
-                T.Resize(int(args.image_size/0.875)),
-                T.CenterCrop(args.image_size),
-                T.ToTensor(),
-                T.Normalize(IMAGENET_MEAN, IMAGENET_STD),
-            ])
-        )
-        val_sampler = DistributedSampler(valset, shuffle=False)
-        val_loader = data.DataLoader(
-            dataset=valset,
-            batch_size=args.batch_size,
-            sampler=val_sampler,
-            num_workers=args.num_workers,
-        )
-    return train_loader, val_loader
+        val_loader = DALIClassificationIterator([val_pipe],
+                                                val_pipe.epoch_size("Reader") / world_size())
+        return val_loader
