364f50f216238e614f2dd540f97bbee19b5f268e
chenyaofo <chenyaofo@gmail.com>
2021-04-11 19:35:57 +0800

    support cifar vit training.

--- codebase/data/cifar.py
+++ codebase/data/cifar.py
@@ -23,6 +23,22 @@ def get_val_transforms(mean, std):
     ])
 
 
+def get_vit_train_transforms(mean, std, img_size):
+    return T.Compose([
+        T.RandomResizedCrop((img_size, img_size), scale=(0.05, 1.0)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+
+def get_vit_val_transforms(mean, std, img_size):
+    return T.Compose([
+        T.Resize((img_size, img_size)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+    ])
+
+
 def get_samplers(trainset, valset):
     if is_dist_avail_and_init():
         train_sampler = DistributedSampler(trainset)
@@ -33,15 +49,19 @@ def get_samplers(trainset, valset):
     return train_sampler, val_sampler
 
 
-@DATA.register
-def cifar10(root, mean, std, batch_size, num_workers, **kwargs):
-    train_transforms = get_train_transforms(mean, std)
-    trainset = CIFAR10(root, train=True, transform=train_transforms, download=True)
+def _cifar(root, image_size, mean, std, batch_size, num_workers, is_vit, dataset_builder, **kwargs):
+    if is_vit:
+        train_transforms = get_vit_train_transforms(mean, std, image_size)
+        val_transforms = get_vit_val_transforms(mean, std, image_size)
+    else:
+        train_transforms = get_train_transforms(mean, std)
+        train_transforms = get_train_transforms(mean, std)
 
-    val_transforms = get_val_transforms(mean, std)
-    valset = CIFAR10(root, train=False, transform=val_transforms, download=True)
+    trainset = dataset_builder(root, train=True, transform=train_transforms, download=True)
+    valset = dataset_builder(root, train=False, transform=val_transforms, download=True)
 
     train_sampler, val_sampler = get_samplers(trainset, valset)
+
     train_loader = data.DataLoader(trainset, batch_size=batch_size,
                                    shuffle=(train_sampler is None),
                                    sampler=train_sampler,
@@ -57,23 +77,14 @@ def cifar10(root, mean, std, batch_size, num_workers, **kwargs):
 
 
 @DATA.register
-def cifar100(root, mean, std, batch_size, num_workers, **kwargs):
-    train_transforms = get_train_transforms(mean, std)
-    trainset = CIFAR100(root, train=True, transform=train_transforms, download=True)
+def cifar10(root, image_size, mean, std, batch_size, num_workers, is_vit, **kwargs):
+    return _cifar(
+        root, image_size, mean, std, batch_size, num_workers, is_vit, CIFAR10, **kwargs
+    )
 
-    val_transforms = get_val_transforms(mean, std)
-    valset = CIFAR100(root, train=False, transform=val_transforms, download=True)
 
-    train_sampler, val_sampler = get_samplers(trainset, valset)
-    train_loader = data.DataLoader(trainset, batch_size=batch_size,
-                                   shuffle=(train_sampler is None),
-                                   sampler=train_sampler,
-                                   num_workers=num_workers,
-                                   persistent_workers=True)
-    val_loader = data.DataLoader(valset, batch_size=batch_size,
-                                 shuffle=(val_sampler is None),
-                                 sampler=val_sampler,
-                                 num_workers=num_workers,
-                                 persistent_workers=True)
-
-    return train_loader, val_loader
+@DATA.register
+def cifar100(root, image_size, mean, std, batch_size, num_workers, is_vit, **kwargs):
+    return _cifar(
+        root, image_size, mean, std, batch_size, num_workers, is_vit, CIFAR100, **kwargs
+    )
