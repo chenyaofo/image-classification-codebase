98535d5eeb11e7eb5021cabfb64d1e8e5f6adb56
chenyaofo <chenyaofo@gmail.com>
2021-10-05 09:45:09 +0800

    add support on arg dis-url with pure domain and ip

--- codebase/config/__init__.py
+++ codebase/config/__init__.py
@@ -1,4 +1,5 @@
 
+import re
 import sys
 import pathlib
 from dataclasses import dataclass
@@ -8,6 +9,21 @@ from pyhocon import ConfigFactory, ConfigTree
 
 from codebase.torchutils.common import get_free_port
 from codebase.torchutils.typed_args import TypedArgs, add_argument
+
+
+def is_valid_domain(value):
+    pattern = re.compile(
+        r'^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|'
+        r'([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|'
+        r'([a-zA-Z0-9][-_.a-zA-Z0-9]{0,61}[a-zA-Z0-9]))\.'
+        r'([a-zA-Z]{2,13}|[a-zA-Z0-9-]{2,30}.[a-zA-Z]{2,3})$'
+    )
+    return True if pattern.match(value) else False
+
+
+def is_valid_ip(str):
+    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    return True if p.match(str) else False
 
 
 @dataclass
@@ -28,6 +44,8 @@ def get_args():
 
     if args.dist_url is None:
         args.dist_url = f"tcp://127.0.0.1:{get_free_port()}"
+    elif is_valid_domain(args.dist_url) or is_valid_ip(args.dist_url):
+        args.dist_url = f"tcp://{args.dist_url}:{get_free_port()}"
 
     args.conf = ConfigFactory.parse_file(args.conf)
     args.output_dir.mkdir(parents=True, exist_ok=args.conf.get_bool("auto_resume"))
