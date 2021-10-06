4dd251c751dc74ac357b88793d3c15a2441ab91d
chenyaofo <chenyaofo@gmail.com>
2021-10-06 22:20:55 +0800

    add resnet18 and resnet50.

--- codebase/models/__init__.py
+++ codebase/models/__init__.py
@@ -1,9 +1,14 @@
 import torch
 import torch.hub
 
+from torchvision.models import resnet18, resnet50
+
 from .register import MODEL
 
 
 @MODEL.register
 def PyTorchHub(repo: str, name: str, **kwargs):
     return torch.hub.load(repo, name, **kwargs)
+
+MODEL.register(resnet18)
+MODEL.register(resnet50)
