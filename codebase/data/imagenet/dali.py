09cda97f149b3ee4ab6880f4009b03b6d9c219a0
chenyaofo <chenyaofo@gmail.com>
2021-10-07 18:12:16 +0800

    remove extra buf size hint.

--- codebase/data/imagenet/dali.py
+++ codebase/data/imagenet/dali.py
@@ -59,19 +59,19 @@ def create_dali_pipeline(reader, image_size, batch_size, mean, std, num_workers,
         dali_device = 'cpu' if dali_cpu else 'gpu'
         decoder_device = 'cpu' if dali_cpu else 'mixed'
         # ask nvJPEG to preallocate memory for the biggest sample in ImageNet for CPU and GPU to avoid reallocations in runtime
-        device_memory_padding = 211025920 if decoder_device == 'mixed' else 0
-        host_memory_padding = 140544512 if decoder_device == 'mixed' else 0
+        # device_memory_padding = 211025920 if decoder_device == 'mixed' else 0
+        # host_memory_padding = 140544512 if decoder_device == 'mixed' else 0
         # ask HW NVJPEG to allocate memory ahead for the biggest image in the data set to avoid reallocations in runtime
-        preallocate_width_hint = 5980 if decoder_device == 'mixed' else 0
-        preallocate_height_hint = 6430 if decoder_device == 'mixed' else 0
+        # preallocate_width_hint = 5980 if decoder_device == 'mixed' else 0
+        # preallocate_height_hint = 6430 if decoder_device == 'mixed' else 0
 
         if is_training:
             images = fn.decoders.image_random_crop(images,
                                                    device=decoder_device, output_type=types.RGB,
-                                                   device_memory_padding=device_memory_padding,
-                                                   host_memory_padding=host_memory_padding,
-                                                   preallocate_width_hint=preallocate_width_hint,
-                                                   preallocate_height_hint=preallocate_height_hint,
+                                                #    device_memory_padding=device_memory_padding,
+                                                #    host_memory_padding=host_memory_padding,
+                                                #    preallocate_width_hint=preallocate_width_hint,
+                                                #    preallocate_height_hint=preallocate_height_hint,
                                                    random_aspect_ratio=[0.75, 4.0 / 3.0],
                                                    random_area=[0.08, 1.0],
                                                    num_attempts=100)
