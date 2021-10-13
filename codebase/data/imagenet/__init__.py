fa5f27f7aff884f438b3a0ec87ded4fb1d070dc3
chenyaofo <chenyaofo@gmail.com>
2021-10-13 19:55:11 +0800

    fix.

--- codebase/data/imagenet/__init__.py
+++ codebase/data/imagenet/__init__.py
@@ -6,10 +6,10 @@ from ..register import DATA
 
 @DATA.register
 def imagenet2012(root, image_size, mean, std, batch_size, num_workers, use_dali,
-                 use_webdataset, trainset_len, valset_len, local_rank, **kwargs):
+                 use_webdataset, use_tfrecord, trainset_len, valset_len, local_rank, **kwargs):
     if use_dali:
         return build_imagenet_dali_loader(root, image_size, mean, std, batch_size, num_workers,
-                                          use_webdataset, trainset_len, valset_len, local_rank)
+                                          use_webdataset, use_tfrecord, trainset_len, valset_len, local_rank)
     else:
         return build_imagenet_loader(root, image_size, mean, std, batch_size, num_workers,
                                      use_webdataset, trainset_len, valset_len)
