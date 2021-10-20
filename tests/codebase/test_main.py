30344fd0aa31340edd49b33ec239304edb12069a
chenyaofo <chenyaofo@gmail.com>
2021-10-20 20:04:06 +0800

    add tests.

---
+++ tests/codebase/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+
+import pkg_resources
+import tempfile
+
+from codebase.config import Args, get_args
+from codebase.main import main
+
+
+
+def test_main():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        args = get_args(["-o", tmpdirname, "--conf", pkg_resources.resource_filename('tests.resources','test.conf')])
+
+        main(args)
