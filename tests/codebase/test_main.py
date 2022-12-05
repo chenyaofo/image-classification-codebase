bef77f6a0f8a090a1e25c7cff7277f3261e643a0
chenyaofo <chenyaofo@gmail.com>
2022-12-05 16:43:33 +0800

    add test.

--- tests/codebase/test_main.py
+++ tests/codebase/test_main.py
@@ -7,9 +7,17 @@ from codebase.config import Args, get_args
 from codebase.main import main
 
 
-
 def test_main():
     with tempfile.TemporaryDirectory() as tmpdirname:
-        args = get_args(["-o", tmpdirname, "--conf", pkg_resources.resource_filename('tests.resources','test.conf')])
+        args = get_args(
+            [
+                "-o",
+                tmpdirname,
+                "--conf",
+                pkg_resources.resource_filename('tests.resources', 'test.conf'),
+                "-M",
+                "max_epochs=3"
+            ]
+        )
 
         main(args)
