cfb87ec7c97a368919b344035244665265ea6428
chenyaofo <chenyaofo@gmail.com>
2023-03-15 21:35:19 +0800

    Update submodule torchutils

--- codebase/criterion/__init__.py
+++ codebase/criterion/__init__.py
@@ -1,6 +1,9 @@
 import torch.nn as nn
 
 from .register import CRITERION
-from .label_smooth import LabelSmoothCrossEntropyLoss
+
 
 CRITERION.register(nn.CrossEntropyLoss)
+
+load_modules(__file__)
+    
